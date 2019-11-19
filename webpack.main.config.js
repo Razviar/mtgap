10df834967ed440981b8d6b47a8b2295c3750f71
Rual Ilmarranen <ilmarranen@gmail.com>
2019-11-19 12:39:25 +0200

    .env

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,25 +1,36 @@
-const path = require("path");
+const path = require('path');
 const ROOT = path.resolve(__dirname);
-const SRC = path.join(ROOT, "src");
-const NODE_MODULES = path.join(ROOT, "node_modules");
-const { TsConfigPathsPlugin } = require("awesome-typescript-loader");
+const SRC = path.join(ROOT, 'src');
+const NODE_MODULES = path.join(ROOT, 'node_modules');
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+const CONFIG = path.join(ROOT, 'conf', `conf.env`);
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: "./src/main.ts",
+  entry: './src/main.ts',
   // Put your normal webpack config below here
   module: {
-    rules: require("./webpack.rules")
+    rules: require('./webpack.rules'),
   },
   resolve: {
     alias: {
-      root: SRC
+      root: SRC,
     },
-    extensions: [".js", ".ts", ".tsx"],
+    extensions: ['.js', '.ts', '.tsx'],
     modules: [NODE_MODULES],
-    plugins: [TsConfigPathsPlugin]
-  }
+    plugins: [TsConfigPathsPlugin],
+  },
+  plugins: [
+    new Dotenv({
+      path: CONFIG,
+      safe: false,
+      systemvars: false,
+      silent: false,
+      defaults: false,
+    }),
+  ],
 };
