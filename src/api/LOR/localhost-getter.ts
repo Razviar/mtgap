81d12dc4961ba18ef8bcc24745cc406240f79b5f
Rual Ilmarranen <ilmarranen@gmail.com>
2021-03-10 14:31:43 +0200

    obsolete files removal

--- src/api/LOR/localhost-getter.ts
+++
@@ -1,6 +0,0 @@
-import {Request} from 'root/app/request';
-
-export async function getLorAPI(endpoint: string): Promise<string> {
-  // console.log(`Getting ${endpoint}`);
-  return Request.get(endpoint, {}, 'http://127.0.0.1:21337/');
-}
